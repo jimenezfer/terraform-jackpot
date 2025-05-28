46057ccd951de96bb5a1bf850c59afefc20f265d
FerJ <delborrows@hotmail.com>
2025-05-27 23:52:32 -0600

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "ami_id" {
+  description = "Amazon Linux 2023 AMI for us-east-1"
+  default     = "ami-0c101f26f147fa7fd"
+}
+
+variable "key_name" {
+  default = "" # Optional if you're not SSHing in
+}
