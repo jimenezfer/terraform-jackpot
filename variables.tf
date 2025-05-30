e94e67fa36a37aeac171388430a440f78c85d56a
FerJ <delborrows@hotmail.com>
2025-05-30 10:57:31 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -13,7 +13,3 @@ variable "ami_id" {
   description = "Amazon Linux 2023 AMI for us-east-1"
   default     = "ami-0c101f26f147fa7fd"
 }
-
-variable "key_name" {
-  default = "" # Optional if you're not SSHing in
-}
